8aae4f0d5a00d8d392f3473e80779c26ae56840d
Luigi Feriani <luigi.feriani@gmail.com>
2020-11-06 11:57:33 +0000

    syngenta library to shuffled stock plates

---
+++ syngenta_library_to_shuffled_stockplates_shuffle1.py
@@ -0,0 +1,200 @@
+"""
+@author lferiani
+@date Jan 14th, 2020
+
+Only use multichannel
+
+Tip rack for drugs (to change) in 3
+Trough for water in 9, well A12 (minimise contamination)
+
+Source 96WPs in 1, 4, 7, 10
+Destination 96WPs in 2, 5, 8, 11
+
+Times 4 (3 plates are full and last plate only has top 4 rows):
+    - Dispense 10 ul of compound from the source (library) 96 WP to its
+        destination (stock) 96 WP (while shuffling columns)
+
+Notation:
+    Source = library
+    Destination = stock
+"""
+
+import numpy as np
+from opentrons import labware, instruments, robot
+
+####################### user intuitive parameters
+
+# multichannel pipette parameters and tipracks
+multi_pipette_type = 'p50-Multi'
+multi_pipette_mount = 'left'
+
+tiprackdrugs_slots = ['3']
+tiprackdrugs_type = 'opentrons-tiprack-300ul'
+tiprackdrugs_startfrom = '1'
+
+# drugs source
+drugs_source_slots = ['10', '7', '4', '1']
+drugs_source_type = '96-well-plate-pcr-thermofisher'
+frombottom_off = +0.5 # mm from bottom of src wells
+drugs_volume = 15.0
+
+
+# destination plates
+destination_slots = ['11', '8', '5', '2']
+destination_type = '96-well-plate-pcr-thermofisher'
+
+n_columns = 12
+
+# create mapping from sources to destination.
+# it is a dict, with:
+# {(source slot, dest slot):(cols in source, cols in dest)}
+drugs_mapping = {}
+seed = 202001241 # for reproducibility. Let's use the experimental date for the actual experiment and the run number, something else for debugging
+np.random.seed(seed)
+src_cols = np.arange(n_columns) # array of column numbers
+for ss, ds in zip(drugs_source_slots, destination_slots):
+    # create entry in dict
+    dst_cols = src_cols.copy() # array of column numbers to be shuffled
+    np.random.shuffle(dst_cols) # shuffle columns in destination. This acts in place!!
+    drugs_mapping[(ss,ds)] = (src_cols,dst_cols)
+
+# print out drugs_mapping:
+for key, value in drugs_mapping.items():
+    _src_slot, _dst_slot = key
+    _src_cols, _dst_cols = value
+    for _src_col, _dst_col in zip(_src_cols, _dst_cols):
+        print('slot {0} col {1} --> slot {2} col {3}'.format(_src_slot, _src_col, _dst_slot, _dst_col))
+
+
+
+############################# define custom multiwell plates
+
+if '48-well-plate-sarsted' not in labware.list():
+    custom_plate = labware.create(
+        '48-well-plate-sarsted',        # name of you labware
+        grid=(8, 6),                    # specify amount of (columns, rows)
+        spacing=(12.4, 12.4),           # distances (mm) between each (column, row)
+        diameter=10,                    # diameter (mm) of each well on the plate
+        depth=17.05,                    # depth (mm) of each well on the plate
+        volume=500)                     # Sarsted had a "volume of work"
+
+    print('Wells in 48WP Sarsted:')
+    for well in custom_plate.wells():
+        print(well)
+
+if '96-well-plate-sqfb-whatman' not in labware.list():
+    custom_plate = labware.create(
+        '96-well-plate-sqfb-whatman',   # name of you labware
+        grid=(12, 8),              # specify amount of (columns, rows)
+        spacing=(8.99, 8.99),      # distances (mm) between each (column, row)
+        diameter=7.57,             # diameter (mm) of each well on the plate (here width at bottom)
+        depth=10.35,               # depth (mm) of each well on the plate
+        volume=650)                # this is the actual volume as per specs, not a "volume of work"
+
+    print('Wells in 96WP Whatman:')
+    for well in custom_plate.wells():
+        print(well)
+
+if '96-well-plate-pcr-thermofisher' not in labware.list():
+    custom_plate = labware.create(
+        '96-well-plate-pcr-thermofisher',   # name of you labware
+        grid=(12, 8),              # specify amount of (columns, rows)
+        spacing=(9.00, 9.00),      # distances (mm) between each (column, row)
+        diameter=5.50,             # diameter (mm) of each well on the plate (here width at top!!)
+        depth=15.00,               # depth (mm) of each well on the plate
+        volume=200)                # as per manufacturer's website
+
+    print('Wells in 96WP PCR Thermo Fisher:')
+    for well in custom_plate.wells():
+        print(well)
+
+############################ define labware
+# i.e. translate user-friendly parameters into opentrons language
+
+# pipette and tiprack
+
+# multi channel
+if multi_pipette_type == 'p50-Multi': # this is mostly a check as we don't own other multichannel pipettes (and to not have swapped single/multi)
+    tiprackdrugs = [labware.load(tiprackdrugs_type, slot) \
+                      for slot in tiprackdrugs_slots]
+    pipette_multi = instruments.P50_Multi(
+        mount=multi_pipette_mount,
+        tip_racks=tiprackdrugs)
+pipette_multi.start_at_tip(tiprackdrugs[0].well(tiprackdrugs_startfrom))
+pipette_multi.plunger_positions['drop_tip'] = -6
+# faster dispense
+pipette_multi.set_speed(dispense=pipette_multi.speeds['dispense']*4)
+
+# translate the drugs mapping dict in robot language
+wells_mapping = {}
+for key, value in drugs_mapping.items():
+    # unpack slots
+    _src_slot, _dst_slot = key
+    _src_cols, _dst_cols = value
+    # load labware for src and dst
+    src_plate = labware.load(drugs_source_type, _src_slot)
+    dst_plate = labware.load(destination_type,  _dst_slot)
+    # create list of wells with the right offset
+    # NOT YET FOLLOWING THE ORDER WE WANT
+    src_wells = [well.bottom(frombottom_off) for well in src_plate.rows('A')]
+    dst_wells = [well.bottom(frombottom_off) for well in dst_plate.rows('A')]
+    # these are normal lists though so should be reshufflable
+    src_wells = [src_wells[wi] for wi in _src_cols]
+    dst_wells = [dst_wells[wi] for wi in _dst_cols]
+    # store in wells_mapping
+    wells_mapping[(src_plate, dst_plate)] = (src_wells, dst_wells)
+
+# print(wells_mapping)
+
+
+################### actions
+# safety command
+pipette_multi.drop_tip()
+
+# stupid function to measure how many tips got used
+def count_used_tips():
+    tc=0
+    for c in robot.commands():
+        if 'Picking up tip wells' in c: # order of checks here matters
+            tc+=8
+        elif 'Picking up tip well' in c: # note the lack of s in well
+            tc+=1
+    print('Tips used so far: {}'.format(tc))
+    return
+
+
+count_used_tips() # should be 0
+
+# first put water, then drugs in plates
+for plates_tuple, wells_tuple in wells_mapping.items():
+    # unpack
+    src_plate, dst_plate = plates_tuple
+    src_wells, dst_wells = wells_tuple
+
+
+    # drug transfer
+    pipette_multi.transfer(drugs_volume,
+                           src_wells,
+                           dst_wells,
+                           new_tip='always',
+                           mix_before=(3, 15),
+                           blow_out=True)
+
+    for s,d in zip(src_wells, dst_wells):
+        print('{} {} -> {} {}'.format(src_plate.parent, s[0], dst_plate.parent, d[0]))
+
+    count_used_tips()
+    robot.pause()
+    pipette_multi.reset_tip_tracking()
+    # each time we fill up a 96wp => expecting 96 tips every plate
+    # so 96, 192, 288, 384
+
+
+#write out robot commands
+if not robot.is_simulating():
+    import datetime
+    out_fname = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")+'_runlog.txt'
+    out_fname = '/data/user_storage/opentrons_data/protocols_logs/' + out_fname
+    with open(out_fname,'w') as fid:
+        for command in robot.commands():
+            print(command,file=fid)
